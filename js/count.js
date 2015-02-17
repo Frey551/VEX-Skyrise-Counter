87f0b37533e79197c7a9ed9e319ad4f304ff50ae
Ariff Asyraf <cyraft551@gmail.com>
2015-02-17 12:23:59 +0800

    Update count.js
    change js script to external

--- js/count.js
+++ js/count.js
@@ -1,1 +1,34 @@
 
+<script type="text/javascript">
+
+
+function IncDecRed(plusminus) {
+
+plusminus = parseInt (plusminus);
+
+currentScore  = parseInt(document.getElementById("scorered").innerHTML) + plusminus;
+
+document.getElementById("scorered").innerHTML = currentScore;
+
+  }
+  
+ function IncDecBlue(plusminus) {
+
+plusminus = parseInt (plusminus);
+
+currentScore  = parseInt(document.getElementById("scoreblue").innerHTML) + plusminus;
+
+document.getElementById("scoreblue").innerHTML = currentScore;
+
+  } 
+ 
+ function reset () {
+
+document.getElementById("scorered").innerHTML = 0;
+document.getElementById("scoreblue").innerHTML = 0; 	
+ 	
+ }
+  
+  
+
+</script>
